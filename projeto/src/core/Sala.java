32e97cf206fe7709e56a51cf300a0f2e37e89def
brunoborgesfurukawa <furukawaborgesbruno@gmail.com>
2018-05-03 19:27:25 -0300

    Iniciando projeto

---
+++ projeto/src/core/Sala.java
@@ -0,0 +1,22 @@
+package core;
+
+public class Sala {
+	
+	protected int nCadeiras;
+	
+
+	public Sala() {
+		
+	}
+	
+	public int getnCadeiras() {
+		return nCadeiras;
+	}
+
+
+	public void setnCadeiras(int nCadeiras) {
+		this.nCadeiras = nCadeiras;
+	}
+	
+
+}
